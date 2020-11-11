c1fa75e0a0f792be172c1715d4d05060bc031e85
BackHoe312 <61866551+BackHoe312@users.noreply.github.com>
2020-11-12 01:02:30 +0900

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,20 @@
-$(window).load(function(){
-    $(window).scroll(function() {
-    var wintop = $(window).scrollTop(), docheight = $('article').height(), winheight = $(window).height();
-    console.log(wintop);
-    var totalScroll = (wintop/(docheight-winheight))*100;
-    console.log("total scroll" + totalScroll);
-    $(".progress-bar").css("width",totalScroll+"%");
-    });
-    
-    });
+window.addEventListener('scroll',function(){
+    var bodyEl = document.querySelector('body');
+    var bodyHeight = bodyEl.offsetHeight;
+    var scrollable = bodyHeight - window.innerHeight;
+    var progressEl = document.querySelector('.progress');
+    var per = Math.floor(window.scrollY / scrollable * 100) + '%';
+    progressEl.style.width = per;
+});
+
+jQuery(document).ready(function($) {
+
+$(".scroll").click(function(event){            
+
+event.preventDefault();
+
+$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+
+});
+
+});
